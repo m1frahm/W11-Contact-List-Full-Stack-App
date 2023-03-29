6e0deb4dcbf181df3fb14f3b879c9108b1c70d49
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-29 17:00:14 -0400

    updated contact to match content of student child component but for contact information instead

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-// import Button from 'react-bootstrap/Button';
-// import * as ioicons from 'react-icons/io5'
+import Button from "react-bootstrap/Button";
+import * as ioicons from "react-icons/io5";
 
-const Contact = (props) => {
+const Contact = ({ contact, toUpdate, toDelete }) => {
+  const onUpdate = (toUpdateContact) => {
+    toUpdate(toUpdateContact);
+  };
+
+  const onDelete = (toDeleteContact) => {
+    toDelete(toDeleteContact);
+  };
+
   return (
     <Card>
       <Card.Body>
         <Card.Title>
-          {props.firstname} {props.lastname}
+          {contact.name} 
         </Card.Title>
-        <Card.Title>{props.email}</Card.Title>
+        <Button
+          variant="outline-danger"
+          onClick={() => {
+            onDelete(contact);
+          }}
+          style={{ padding: "0.6em", marginRight: "0.9em" }}
+        >
+          <ioicons.IoTrash />
+        </Button>
+        <Button
+          variant="outline-info"
+          onClick={() => {
+            onUpdate(contact);
+          }}
+          style={{ padding: "0.6em" }}
+        >
+          {" "}
+          <ioicons.IoSync />
+        </Button>
       </Card.Body>
     </Card>
   );
